835a8efcaa1e64c4e0e1da9c1ebfd8e510d42c55
Ryan Crowley <ryancrowleycode@gmail.com>
2019-10-25 11:08:45 -0500

    wrote fetch call, which has a .then statement that calls the function to print the journal entries retreived in the fetch call to the DOM.

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,7 +1,17 @@
-
 // a reference to the article tag that will contain my journal entries on the DOM
 const domJournalContainer = document.querySelector(".entryLog")
 
+// function with fetch call to entries.json
+const getEntries = () => {
+    fetch("http://localhost:3000/entries") // gets the json
+    .then(response => response.json()) // turns the json into javascript object
+    .then(entries => {
+        renderJournalEntries(entries) // calls the function to print each entry in entries to the DOM
+    })
+}
+
+// calling the fetch call function
+getEntries();
 
 
 // This function takes a journalEntry object and returns a string template that represents a single journal entry object as HTML.
@@ -25,5 +35,3 @@ const renderJournalEntries = (entries) => {
     }
     )}
 
-// Calling my function to put the journal entries on the DOM
-renderJournalEntries(journalCollection)
