18a29b055e4ecf097d060e006e2456cfa5eab301
Ryan Crowley <ryancrowleycode@gmail.com>
2019-10-25 11:41:34 -0500

    journal five up to the challenge is complete. Need to do challenge on journal 5

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,11 +1,9 @@
 // function with fetch call to entries.json
-const getEntries = () => {
-    fetch("http://localhost:3000/entries") // gets the json
-    .then(response => response.json()) // turns the json into javascript object
-    .then(entries => {
-        renderJournalEntries(entries) // calls the function to print each entry in entries to the DOM
-    })
+const API = {
+    getEntries: function () {
+        return fetch("http://localhost:3000/entries") // gets the json
+        .then(response => response.json()) // turns the json into javascript object
+
+    }
 }
 
-// calling the fetch call function
-getEntries();
