ec7659168879f0bbf71ac137221f1b94da92dbf9
Ryan Crowley <ryancrowleycode@gmail.com>
2019-11-01 13:57:19 -0500

    in eventManager.js, I have a function that loops through the mood radio buttons and adds an event listener to each.

--- src/scripts/eventManager.js
+++ src/scripts/eventManager.js
@@ -9,37 +9,51 @@ const getAndRender = () => {
 
 const Events = {
     recordEntry: function (date, concepts, entry, mood) {
-            // variable to get the actual text inside the mood, not just the value, like "Great!"
-            const moodText = mood.options[mood.selectedIndex].text
-            
-            const inputFieldArray = [date, concepts, entry, mood]
-            
-            // checking to make sure all forms are filled out before posting to database
-            if (date.value, concepts.value, entry.value, mood.value) { 
-                // checking to make sure concepts field matches the required format
-                if (concepts.checkValidity()) {
-                    // Posting to API
-                    const newestEntry = NewJournal.createJournalObject(date.value, concepts.value, entry.value, moodText)
-                    API.saveJournalEntry(newestEntry)
-                    .then(getAndRender)
-                    
-                    // for loop to clear inputFields
-                    inputFieldArray.forEach(inputField => {
-                        inputField.value = ""
-                    })
-                    
-                } else {
-                    window.alert(concepts.title)
-                }
+        // variable to get the actual text inside the mood, not just the value, like "Great!"
+        const moodText = mood.options[mood.selectedIndex].text
+        
+        const inputFieldArray = [date, concepts, entry, mood]
+        
+        // checking to make sure all forms are filled out before posting to database
+        if (date.value, concepts.value, entry.value, mood.value) { 
+            // checking to make sure concepts field matches the required format
+            if (concepts.checkValidity()) {
+                // Posting to API
+                const newestEntry = NewJournal.createJournalObject(date.value, concepts.value, entry.value, moodText)
+                API.saveJournalEntry(newestEntry)
+                .then(getAndRender)
+                
+                // for loop to clear inputFields
+                inputFieldArray.forEach(inputField => {
+                    inputField.value = ""
+                })
+                
             } else {
-                window.alert("Please fill out all forms for the journal before recording your entry.")
+                window.alert(concepts.title)
             }
-        },
+        } else {
+            window.alert("Please fill out all forms for the journal before recording your entry.")
+        }
+    },
 
-    listenToRecordButtonClick: function (recButton, date, concepts, entry, mood) {
+    listenToRecordButtonClick: function (date, concepts, entry, mood) {
+        // targeting the record button
+        const recButton = document.querySelector("#recordButton")
+
         recButton.addEventListener("click", () => {
             this.recordEntry(date, concepts, entry, mood)
         })
+    },
+
+    listenToMoodButtons: function () {
+        const moodButtons = document.getElementsByName("mood-radio-button")
+        moodButtons.forEach( moodButton => {
+            moodButton.addEventListener("click", () => {
+                const selectedMood = event.target.value
+                console.log(selectedMood)
+            })
+        })
+
     }
 }
 
