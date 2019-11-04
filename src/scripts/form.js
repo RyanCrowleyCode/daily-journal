a65b6122569881bd04c35ab1999313ec6f393f40
Ryan Crowley <ryancrowleycode@gmail.com>
2019-11-04 14:03:34 -0600

    delete button created with styling for each entry

--- src/scripts/form.js
+++ src/scripts/form.js
@@ -1,4 +1,3 @@
-// Challenge: Entry Form Component
 // Create a module that defines a function for building the form fields dynamically instead of them being hard-coded in the HTML.
 
 const journalForm = `
